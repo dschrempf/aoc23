a770273742bb6b3f6a62fc9a64528d358aec9d92
Dominik Schrempf <dominik.schrempf@gmail.com>
2023-12-01 08:30:10 +0100

    day 1

--- app/Day01.hs
+++ app/Day01.hs
@@ -17,27 +17,49 @@ module Main
 where
 
 import Aoc
-import Control.Applicative
-import Data.Attoparsec.Text (Parser, decimal, endOfLine, sepBy1', space)
-import Data.List (sortBy)
-import Data.Ord (Down (Down), comparing)
+import Aoc.Char (readDigit)
+import Control.Applicative (asum)
+import Data.Attoparsec.Text (Parser, endOfInput, endOfLine, isEndOfLine, sepBy1', takeTill)
+import Data.Char (isDigit)
+import Data.List (tails)
+import Data.Maybe (fromJust)
+import qualified Data.Text as T
 
-pElf :: Parser [Int]
-pElf = decimal `sepBy1'` endOfLine
+pLine :: Parser Text
+pLine = takeTill isEndOfLine
 
-pElfs :: Parser [[Int]]
-pElfs = pElf `sepBy1'` some space
+pInput :: Parser [Text]
+pInput = init <$> (pLine `sepBy1'` endOfLine <* endOfInput)
 
-solve :: [[Int]] -> Int
-solve = maximum . map sum
+solveLn :: Text -> Int
+solveLn = read . (\xs -> [head xs, last xs]) . filter isDigit . T.unpack
 
-solve2 :: [[Int]] -> Int
-solve2 = sum . take 3 . sortBy (comparing Data.Ord.Down) . map sum
+pDigit :: String -> Maybe Int
+pDigit xs
+  | take 3 xs == "one" = Just 1
+  | take 3 xs == "two" = Just 2
+  | take 5 xs == "three" = Just 3
+  | take 4 xs == "four" = Just 4
+  | take 4 xs == "five" = Just 5
+  | take 3 xs == "six" = Just 6
+  | take 5 xs == "seven" = Just 7
+  | take 5 xs == "eight" = Just 8
+  | take 4 xs == "nine" = Just 9
+  | null xs = Nothing
+  | isDigit $ head xs = Just $ readDigit $ head xs
+  | otherwise = Nothing
+
+getFirst :: String -> Int
+getFirst xs = fromJust $ asum $ map pDigit $ tails xs
+
+getLast :: String -> Int
+getLast xs = fromJust $ asum $ map pDigit $ reverse $ tails xs
 
 main :: IO ()
 main = do
-  d <- parseChallengeT (F 1) pElfs
-  let s = solve d
-  print s
-  let s2 = solve2 d
-  print s2
+  d <- parseChallengeT (F 1) pInput
+  print d
+  print $ sum $ map solveLn d
+  let xs = map (getFirst . T.unpack) d
+      ys = map (getLast . T.unpack) d
+  print $ sum $ zipWith (\x y -> (10 * x) + y) xs ys
