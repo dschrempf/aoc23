660459376d07e69b2c87b3159299b5950f0c5025
Dominik Schrempf <dominik.schrempf@gmail.com>
2023-12-04 13:12:12 +0100

    day04, part1

--- app/Day04.hs
+++ app/Day04.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      :  Main
 -- Description :  Day 4
@@ -16,5 +18,48 @@ module Main
   )
 where
 
+import Aoc (parseChallengeT)
+import Aoc.Def (Challenge (..))
+import Data.Attoparsec.Text
+  ( Parser,
+    char,
+    decimal,
+    endOfInput,
+    endOfLine,
+    sepBy1',
+    skipSpace,
+    string,
+  )
+import qualified Data.Set as S
+
+data Card = Card {number :: Int, wins :: S.Set Int, picks :: S.Set Int}
+  deriving (Show)
+
+pCard :: Parser Card
+pCard = do
+  _ <- string "Card"
+  skipSpace
+  n <- decimal
+  _ <- char ':'
+  skipSpace
+  ws <- decimal `sepBy1'` skipSpace
+  skipSpace
+  _ <- char '|'
+  skipSpace
+  ps <- decimal `sepBy1'` skipSpace
+  return $ Card n (S.fromList ws) (S.fromList ps)
+
+pInput :: Parser [Card]
+pInput = pCard `sepBy1'` endOfLine <* endOfLine <* endOfInput
+
+winningPicks :: Card -> S.Set Int
+winningPicks (Card _ ws ps) = S.intersection ws ps
+
+gradeIntersectionPart1 :: S.Set Int -> Int
+gradeIntersectionPart1 xs
+  | null xs = 0
+  | otherwise = 2 ^ pred (S.size xs)
 main :: IO ()
-main = undefined
+main = do
+  d <- parseChallengeT (Full 4) pInput
+  print $ sum $ map (gradeIntersectionPart1 . winningPicks) d
