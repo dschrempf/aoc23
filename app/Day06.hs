c94c75b1cbb7346f9c015a6ee5963afae5c2c9f6
Dominik Schrempf <dominik.schrempf@gmail.com>
2023-12-06 06:32:32 +0100

    day 6

--- app/Day06.hs
+++ app/Day06.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      :  Main
 -- Description :  Day 6
@@ -16,5 +18,54 @@ module Main
   )
 where
 
+import Aoc (parseChallengeT)
+import Aoc.Def (Challenge (..))
+import Data.Attoparsec.Text (Parser, decimal, endOfInput, endOfLine, sepBy1', skipSpace, string)
+
+qu :: Int -> Int -> (Int, Int)
+qu tm' sm' = (floor $ 0.5 * tm - c + 1, ceiling $ 0.5 * tm + c - 1)
+  where
+    tm :: Double
+    tm = fromIntegral tm'
+    sm :: Double
+    sm = fromIntegral sm'
+    c = 0.5 * sqrt (tm * tm - 4 * sm)
+
+intervalLength :: (Int, Int) -> Int
+intervalLength (a, b) = b - a + 1
+
+pInput :: Parser ([Int], [Int])
+pInput = do
+  _ <- string "Time:"
+  skipSpace
+  ts <- decimal `sepBy1'` skipSpace
+  endOfLine
+  _ <- string "Distance:"
+  skipSpace
+  ds <- decimal `sepBy1'` skipSpace
+  endOfLine
+  endOfInput
+  return (ts, ds)
+
+nWays :: Int -> Int -> Int
+nWays tm sm = intervalLength $ qu tm sm
+
+solve :: ([Int], [Int]) -> [Int]
+solve (xs, ys) = zipWith nWays xs ys
+
+rmSpace :: String -> String
+rmSpace [] = []
+rmSpace (' ' : xs) = rmSpace xs
+rmSpace (x : xs) = x : rmSpace xs
+
+conc :: [Int] -> Int
+conc = read . rmSpace . unwords . map show
+
+solve2 :: ([Int], [Int]) -> Int
+solve2 (xs, ys) = nWays (conc xs) (conc ys)
+
 main :: IO ()
-main = undefined
+main = do
+  d <- parseChallengeT (Full 6) pInput
+  print $ product $ solve d
+  print $ solve2 d
