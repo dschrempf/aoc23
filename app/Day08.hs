06281e6013600983657a9b38e5b52e7a798e8484
Dominik Schrempf <dominik.schrempf@gmail.com>
2023-12-08 21:33:29 +0100

    day 8 part 1

--- app/Day08.hs
+++ app/Day08.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 -- |
 -- Module      :  Main
 -- Description :  Day 8
@@ -16,5 +18,82 @@ module Main
   )
 where
 
+import Aoc (parseChallengeT)
+import Aoc.Def (Challenge (..))
+import Control.Applicative (Alternative (..))
+import Data.Attoparsec.Text (Parser, char, count, endOfInput, endOfLine, letter, sepBy1', skipSpace)
+import qualified Data.Map.Strict as M
+
+data Direction = L | R
+  deriving (Show, Eq)
+
+pDirs :: Parser [Direction]
+pDirs = some dir
+  where
+    r = R <$ char 'R'
+    l = L <$ char 'L'
+    dir = r <|> l
+
+type Node = String
+
+pNode :: Parser Node
+pNode = count 3 letter
+
+data Destination = Destination {left :: Node, right :: Node}
+  deriving (Show)
+
+pDestination :: Parser Destination
+pDestination =
+  Destination
+    <$> (char '(' *> pNode <* char ',' <* skipSpace)
+    <*> (pNode <* char ')')
+
+data Edge = Edge {start :: Node, destination :: Destination}
+  deriving (Show)
+
+pEdge :: Parser Edge
+pEdge =
+  Edge
+    <$> (pNode <* skipSpace <* char '=' <* skipSpace)
+    <*> pDestination
+
+pInput :: Parser ([Direction], [Edge])
+pInput = do
+  dirs <- pDirs
+  endOfLine
+  endOfLine
+  edges <- pEdge `sepBy1'` endOfLine
+  endOfLine
+  endOfInput
+  pure (dirs, edges)
+
+type Network = M.Map Node Destination
+
+toNetwork :: [Edge] -> Network
+toNetwork = foldl insertEdge M.empty
+  where
+    insertEdge m (Edge s d) = M.insert s d m
+
+getDestination :: Direction -> Destination -> Node
+getDestination L = left
+getDestination R = right
+
+jump :: Direction -> Network -> Node -> Node
+jump dir net node = getDestination dir $ net M.! node
+
+walk :: Node -> Node -> [Direction] -> Network -> Int
+walk s e = go s 1
+  where
+    go n l (d : ds) net =
+      let !n' = jump d net n
+       in if n' == e
+            then l
+            else let !l' = succ l in go n' l' ds net
+    go _ _ _ _ = error "out of nodes or directions"
+
 main :: IO ()
-main = undefined
+main = do
+  (dirs, edges) <- parseChallengeT (Full 8) pInput
+  let net = toNetwork edges
+      l = walk "AAA" "ZZZ" (cycle dirs) net
+  print l
