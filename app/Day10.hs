04acf7988d2619aaa845d461d4917bd85be6077b
Dominik Schrempf <dominik.schrempf@gmail.com>
2023-12-10 20:45:37 +0100

    day 10, part 2; and some refactoring

--- app/Day10.hs
+++ app/Day10.hs
@@ -20,14 +20,17 @@ where
 
 import Aoc (Challenge (..), parseChallengeT)
 import Aoc.Array (filterA, neighborsNoDiagonal)
+import Aoc.Bounded (predWrap, succWrap)
+import Aoc.Direction (Direction, directionToIx2, ix2ToDirections)
 import Aoc.Set (flatten)
 import Control.Applicative (Alternative (..))
 import Control.DeepSeq (force)
 import Data.Attoparsec.Text (Parser, char, choice, endOfLine, sepBy1')
-import Data.Massiv.Array (Array, B, Comp (..), Ix2 (..), (!))
+import Data.Bifunctor (Bifunctor (..))
+import Data.List (nub)
+import Data.Massiv.Array (Array, B, Comp (..), Ix2 (..), Sz (..), (!))
 import qualified Data.Massiv.Array as A
 import qualified Data.Set as S
-import Debug.Trace (traceShowId)
 
 data Tile
   = Ground
@@ -83,22 +86,84 @@ step m ls = flatten . S.map (dontStepBack . stepSingle)
     stepSingle x = S.filter (isConnected m x) $ connectsTo m x
     dontStepBack = S.filter (`notElem` ls)
 
-stepN :: Map -> S.Set Ix2 -> (S.Set Ix2, Int)
-stepN m = go 0 S.empty S.empty
+findNumberOfSteps :: Map -> S.Set Ix2 -> Int
+findNumberOfSteps m = go 0 S.empty
   where
-    go n visited lasts currents =
-      let visited' = S.union visited currents
-          lasts' = currents
+    go n lasts currents =
+      let lasts' = currents
           currents' = force $ step m lasts currents
           !n' = succ n
        in if length currents' <= 1
-            then let allVisited = S.union visited' currents' in (allVisited, n')
-            else go n' visited' lasts' currents'
+            then n'
+            else go n' lasts' currents'
+
+firstStep :: Map -> Ix2 -> Ix2
+firstStep m s = head $ S.toList $ step m S.empty (S.singleton s)
+
+findPipeLoop :: Map -> Ix2 -> [Ix2]
+findPipeLoop m s = reverse $ go [s] s (firstStep m s)
+  where
+    go visitedIxs lastIx currentIx =
+      let visitedIxs' = currentIx : visitedIxs
+          lastIx' = currentIx
+          currentIx' =
+            force $
+              head $
+                S.toList $
+                  step m (S.singleton lastIx) (S.singleton currentIx)
+       in if currentIx' == s
+            then currentIx' : visitedIxs'
+            else go visitedIxs' lastIx' currentIx'
+
+getLoopWithDirections :: [Ix2] -> [(Ix2, Direction)]
+getLoopWithDirections (x : y : z : xs) = [(y, d) | d <- directions] ++ goOn
+  where
+    directions = ix2ToDirections $ z - x
+    goOn = getLoopWithDirections (y : z : xs)
+getLoopWithDirections _ = []
+
+raytraceUntil :: Sz Ix2 -> S.Set Ix2 -> Ix2 -> Direction -> [Ix2]
+raytraceUntil sz lp ix dir
+  | ix' `S.member` lp || not (A.isSafeIndex sz ix') = []
+  | otherwise = ix' : raytraceUntil sz lp ix' dir
+  where
+    ix' = ix + directionToIx2 dir
+
+ixsRightOf :: Sz Ix2 -> S.Set Ix2 -> Ix2 -> Direction -> [Ix2]
+ixsRightOf sz lp ix dir = raytraceUntil sz lp ix (succWrap dir)
+
+ixsLeftOf :: Sz Ix2 -> S.Set Ix2 -> Ix2 -> Direction -> [Ix2]
+ixsLeftOf sz lp ix dir = raytraceUntil sz lp ix (predWrap dir)
+
+getRightAndLeftTiles :: Sz Ix2 -> S.Set Ix2 -> [(Ix2, Direction)] -> ([Ix2], [Ix2])
+getRightAndLeftTiles sz lp = go [] []
+  where
+    go lefts rights [] = (lefts, rights)
+    go lefts rights ((x, dir) : xs) =
+      go
+        (ixsLeftOf sz lp x dir ++ lefts)
+        (ixsRightOf sz lp x dir ++ rights)
+        xs
+
+atBorder :: Sz Ix2 -> Ix2 -> Bool
+atBorder (Sz (rows :. cols)) (m :. n) = m == 0 || n == 0 || m == pred rows || n == pred cols
+
+isOutside :: Sz Ix2 -> [Ix2] -> Bool
+isOutside sz = any (atBorder sz)
 
 main :: IO ()
 main = do
-  m <- parseChallengeT (Sample 10 13) pMap
+  m <- parseChallengeT (Full 10) pMap
   let s = findStart m
-      (loop, n) = stepN m $ S.singleton s
-  print loop
-  print n
+  print $ findNumberOfSteps m $ S.singleton s
+  let pipeLoop = findPipeLoop m s
+      pipeLoopSet = S.fromList pipeLoop
+      loopWithDirections = getLoopWithDirections $ last pipeLoop : pipeLoop
+      mapSize = A.size m
+      (leftTiles, rightTiles) =
+        bimap nub nub $
+          getRightAndLeftTiles mapSize pipeLoopSet loopWithDirections
+  print $ length $ case (isOutside mapSize leftTiles, isOutside mapSize rightTiles) of
+    (True, False) -> rightTiles
+    (False, True) -> leftTiles
+    _ -> error "could not determine inside"
