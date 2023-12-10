38f21b5a7573b55ccee80181b2779b8d32b55ec3
Dominik Schrempf <dominik.schrempf@gmail.com>
2023-12-10 13:49:02 +0100

    day 10, part 1 with sets

--- app/Day10.hs
+++ app/Day10.hs
@@ -20,12 +20,13 @@ where
 
 import Aoc (Challenge (..), parseChallengeT)
 import Aoc.Array (filterA, neighborsNoDiagonal)
+import Aoc.Set (flatten)
 import Control.Applicative (Alternative (..))
 import Control.DeepSeq (force)
 import Data.Attoparsec.Text (Parser, char, choice, endOfLine, sepBy1')
-import Data.List
 import Data.Massiv.Array (Array, B, Comp (..), Ix2 (..), (!))
 import qualified Data.Massiv.Array as A
+import qualified Data.Set as S
 import Debug.Trace (traceShowId)
 
 data Tile
@@ -59,8 +60,8 @@ pMap = A.fromLists' Seq <$> some pPipe `sepBy1'` endOfLine
 findStart :: Map -> Ix2
 findStart = head . filterA (== Start)
 
-connectsTo :: Map -> Ix2 -> [Ix2]
-connectsTo m ix = filter (A.isSafeIndex $ A.size m) $ case m ! ix of
+connectsTo :: Map -> Ix2 -> S.Set Ix2
+connectsTo m ix = S.filter (A.isSafeIndex $ A.size m) $ S.fromList $ case m ! ix of
   Ground -> []
   Start -> neighborsNoDiagonal (A.size m) ix
   Vertical -> [ix + northSouth, ix - northSouth]
@@ -76,26 +77,28 @@ connectsTo m ix = filter (A.isSafeIndex $ A.size m) $ case m ! ix of
 isConnected :: Map -> Ix2 -> Ix2 -> Bool
 isConnected m ix1 ix2 = ix2 `elem` connectsTo m ix1 && ix1 `elem` connectsTo m ix2
 
-step :: Map -> [Ix2] -> [Ix2] -> [Ix2]
-step m ls = nub . concatMap (dontStepBack . stepSingle)
+step :: Map -> S.Set Ix2 -> S.Set Ix2 -> S.Set Ix2
+step m ls = flatten . S.map (dontStepBack . stepSingle)
   where
-    stepSingle x = filter (isConnected m x) $ connectsTo m x
-    dontStepBack = filter (`notElem` ls)
+    stepSingle x = S.filter (isConnected m x) $ connectsTo m x
+    dontStepBack = S.filter (`notElem` ls)
 
-stepN :: Map -> [Ix2] -> Int
-stepN m = go 0 []
+stepN :: Map -> S.Set Ix2 -> (S.Set Ix2, Int)
+stepN m = go 0 S.empty S.empty
   where
-    go n ls xs =
-      let xs' = traceShowId $ force $ step m ls xs
-          !n' = traceShowId $ succ n
-       in if length xs' <= 1
-            then n'
-            else go n' xs xs'
+    go n visited lasts currents =
+      let visited' = S.union visited currents
+          lasts' = currents
+          currents' = force $ step m lasts currents
+          !n' = succ n
+       in if length currents' <= 1
+            then let allVisited = S.union visited' currents' in (allVisited, n')
+            else go n' visited' lasts' currents'
 
 main :: IO ()
 main = do
   m <- parseChallengeT (Sample 10 13) pMap
   let s = findStart m
-  print m
-  print s
-  print $ stepN m [s]
+      (loop, n) = stepN m $ S.singleton s
+  print loop
+  print n
