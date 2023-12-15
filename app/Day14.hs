eb72b2883146a0a54bb98a72403c355ed88ec515
Dominik Schrempf <dominik.schrempf@gmail.com>
2023-12-15 14:21:41 +0100

    day 14, parsing

--- app/Day14.hs
+++ app/Day14.hs
@@ -16,5 +16,32 @@ module Main
   )
 where
 
+import Aoc (parseChallengeT)
+import Aoc.Array (pMatrix)
+import Aoc.Def
+import Data.Attoparsec.Text (Parser)
+import Data.List (singleton)
+import Data.Massiv.Array (Array, B, Ix2)
+
+-- import qualified Data.Massiv.Array as A
+
+data Position = Rolling | Fixed | Empty
+  deriving (Eq)
+
+charMap :: Position -> Char
+charMap Rolling = 'O'
+charMap Fixed = '#'
+charMap Empty = '.'
+
+instance Show Position where
+  show = singleton . charMap
+
+type Field = Array B Ix2 Position
+
+pField :: Parser Field
+pField = pMatrix [('O', Rolling), ('#', Fixed), ('.', Empty)]
+
 main :: IO ()
-main = undefined
+main = do
+  f <- parseChallengeT (Sample 14 1) pField
+  print f
