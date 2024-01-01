2d992c30c25b5997b9a4b83bf5bdf3e2105a8cd7
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-01-01 17:48:34 +0100

    day 18, parser

--- app/Day18.hs
+++ app/Day18.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      :  Main
 -- Description :  Day 18
@@ -16,5 +18,61 @@ module Main
   )
 where
 
+import Aoc
+import Aoc.Direction (Direction (..))
+import Aoc.Parse (skipHorizontalSpace)
+import Data.Attoparsec.Text
+  ( Parser,
+    char,
+    choice,
+    decimal,
+    endOfInput,
+    endOfLine,
+    hexadecimal,
+    sepBy1',
+    string,
+  )
+import Data.Word (Word8)
+import GHC.Natural (Natural)
+
+type RGB = (Word8, Word8, Word8)
+
+data Instruction = Instruction {direction :: Direction, edgeLength :: Natural, rgb :: RGB}
+  deriving (Eq, Show)
+
+type DigPlan = [Instruction]
+
+pDirection :: Parser Direction
+pDirection =
+  choice
+    [ North <$ char 'U',
+      East <$ char 'R',
+      South <$ char 'D',
+      West <$ char 'L'
+    ]
+
+pColor :: Parser RGB
+pColor = do
+  _ <- string "(#"
+  c <- hexadecimal :: Parser Int
+  _ <- char ')'
+  let b = c `mod` 256
+      g = ((c - b) `quot` 256) `mod` 256
+      r = ((c - b) `quot` (256 * 256)) `mod` 256
+  pure (fromIntegral r, fromIntegral g, fromIntegral b)
+
+pInstruction :: Parser Instruction
+pInstruction = do
+  d <- pDirection
+  _ <- skipHorizontalSpace
+  l <- decimal
+  _ <- skipHorizontalSpace
+  Instruction d l <$> pColor
+
+pDigPlan :: Parser DigPlan
+pDigPlan = pInstruction `sepBy1'` endOfLine <* endOfLine <* endOfInput
+
 main :: IO ()
-main = undefined
+main = do
+  d <- parseChallengeT (Sample 18 1) pDigPlan
+  print d
