8ce2d09ac819e5fbdac7399b9814e6c18c77196b
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-01-01 21:24:48 +0100

    day 18, part 1

--- app/Day18.hs
+++ app/Day18.hs
@@ -19,7 +19,8 @@ module Main
 where
 
 import Aoc
-import Aoc.Direction (Direction (..))
+import Aoc.Bounded (predWrap, succWrap)
+import Aoc.Direction (Direction (..), directionToIx2, ix2ToDirections, moveNStepsInDirection)
 import Aoc.Parse (skipHorizontalSpace)
 import Data.Attoparsec.Text
   ( Parser,
@@ -32,6 +33,12 @@ import Data.Attoparsec.Text
     sepBy1',
     string,
   )
+import Data.Bifunctor (Bifunctor (..))
+import Data.List (nub)
+import Data.Massiv.Array (Ix2 (..), Sz (..))
+import qualified Data.Massiv.Array as A
+import Data.Set (Set)
+import qualified Data.Set as S
 import Data.Word (Word8)
 import GHC.Natural (Natural)
 
@@ -39,8 +46,6 @@ type RGB = (Word8, Word8, Word8)
 
 data Instruction = Instruction {direction :: Direction, edgeLength :: Natural, rgb :: RGB}
   deriving (Eq, Show)
-
-type DigPlan = [Instruction]
 
 pDirection :: Parser Direction
 pDirection =
@@ -69,10 +74,83 @@ pInstruction = do
   _ <- skipHorizontalSpace
   Instruction d l <$> pColor
 
-pDigPlan :: Parser DigPlan
+pDigPlan :: Parser [Instruction]
 pDigPlan = pInstruction `sepBy1'` endOfLine <* endOfLine <* endOfInput
+
+type Trench = [(Ix2, RGB)]
+
+excavate :: [Instruction] -> Trench
+excavate = snd . foldl accF (0 :. 0, [])
+  where
+    accF (pos, trench) (Instruction dir len col) =
+      let poss = getPositions pos dir len
+          trench' = trench ++ [(p, col) | p <- getPositions pos dir len]
+       in (last poss, trench')
+    getPositions pos dir len =
+      [ moveNStepsInDirection (fromIntegral n) pos dir
+        | n <- [1 .. len]
+      ]
+
+getDimensions :: Trench -> (Ix2, Ix2)
+getDimensions xs = (topLeft, bottomRight)
+  where
+    ks = map fst xs
+    ms = map (fst . A.fromIx2) ks
+    ns = map (snd . A.fromIx2) ks
+    topLeft = minimum ms :. minimum ns
+    bottomRight = maximum ms :. maximum ns
+
+getLoopWithDirections :: [Ix2] -> [(Ix2, Direction)]
+getLoopWithDirections (x : y : z : xs) = [(y, d) | d <- directions] ++ goOn
+  where
+    directions = ix2ToDirections $ z - x
+    goOn = getLoopWithDirections (y : z : xs)
+getLoopWithDirections _ = []
+
+raytraceUntil :: Sz Ix2 -> Set Ix2 -> Ix2 -> Direction -> [Ix2]
+raytraceUntil sz lp ix dir
+  | ix' `S.member` lp || not (A.isSafeIndex sz ix') = []
+  | otherwise = ix' : raytraceUntil sz lp ix' dir
+  where
+    ix' = ix + directionToIx2 dir
+
+ixsRightOf :: Sz Ix2 -> Set Ix2 -> Ix2 -> Direction -> [Ix2]
+ixsRightOf sz lp ix dir = raytraceUntil sz lp ix (succWrap dir)
+
+ixsLeftOf :: Sz Ix2 -> Set Ix2 -> Ix2 -> Direction -> [Ix2]
+ixsLeftOf sz lp ix dir = raytraceUntil sz lp ix (predWrap dir)
+
+getRightAndLeftTiles :: Sz Ix2 -> Set Ix2 -> [(Ix2, Direction)] -> ([Ix2], [Ix2])
+getRightAndLeftTiles sz lp = go [] []
+  where
+    go lefts rights [] = (lefts, rights)
+    go lefts rights ((x, dir) : xs) =
+      go
+        (ixsLeftOf sz lp x dir ++ lefts)
+        (ixsRightOf sz lp x dir ++ rights)
+        xs
+
+atBorder :: Sz Ix2 -> Ix2 -> Bool
+atBorder (Sz (rows :. cols)) (m :. n) = m == 0 || n == 0 || m == pred rows || n == pred cols
+
+isOutside :: Sz Ix2 -> [Ix2] -> Bool
+isOutside sz = any (atBorder sz)
 
 main :: IO ()
 main = do
-  d <- parseChallengeT (Sample 18 1) pDigPlan
-  print d
+  is <- parseChallengeT (Full 18) pDigPlan
+  let trenchShifted = excavate is
+      (topLeftShifted, _) = getDimensions trenchShifted
+      trench = map (\(k, c) -> (k - topLeftShifted, c)) trenchShifted
+      (_, bottomRight) = getDimensions trench
+      trenchSize = Sz $ bottomRight + (1 :. 1)
+      trenchPath = map fst trench
+      trenchPathLoop = last trenchPath : trenchPath
+      trenchPathLoopWithDirection = getLoopWithDirections trenchPathLoop
+      (leftTiles, rightTiles) =
+        bimap nub nub $
+          getRightAndLeftTiles trenchSize (S.fromList trenchPathLoop) trenchPathLoopWithDirection
+  print $ (+ length trench) $ length $ case (isOutside trenchSize leftTiles, isOutside trenchSize rightTiles) of
+    (True, False) -> rightTiles
+    (False, True) -> leftTiles
+    _ -> error "could not determine inside"
