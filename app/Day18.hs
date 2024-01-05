95ce437f9d1f231cc04ed501000642678f867c80
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-01-05 23:17:02 +0100

    towards day 18, part 2

--- app/Day18.hs
+++ app/Day18.hs
@@ -112,11 +112,11 @@ raytraceUntil sz lp ix dir
   where
     ix' = ix + directionToIx2 dir
 
-ixsRightOf :: Sz Ix2 -> Set Ix2 -> Ix2 -> Direction -> [Ix2]
-ixsRightOf sz lp ix dir = raytraceUntil sz lp ix (succWrap dir)
-
 ixsLeftOf :: Sz Ix2 -> Set Ix2 -> Ix2 -> Direction -> [Ix2]
 ixsLeftOf sz lp ix dir = raytraceUntil sz lp ix (predWrap dir)
+
+ixsRightOf :: Sz Ix2 -> Set Ix2 -> Ix2 -> Direction -> [Ix2]
+ixsRightOf sz lp ix dir = raytraceUntil sz lp ix (succWrap dir)
 
 getRightAndLeftTiles :: Sz Ix2 -> Set Ix2 -> [(Ix2, Direction)] -> ([Ix2], [Ix2])
 getRightAndLeftTiles sz lp = go [] []
@@ -188,32 +188,64 @@ excavate2 = foldl accF [0 :. 0]
 slices :: Int -> Int -> Int -> Bool
 slices x a b = (a < x && x < b) || (a > x && x > b)
 
--- Assume ms are sorted.
-sliceTrenchH :: [Int] -> Trench -> Trench
-sliceTrenchH ms ((m1 :. n1) : (m2 :. n2) : xs) =
-  (m1 :. n1) : additionalPoints ++ sliceTrenchH ms ((m2 :. n2) : xs)
-  where
-    msSlicing = filter (\m -> slices m m1 m2) ms
-    additionalPoints = [m :. n1 | m <- msSlicing]
-sliceTrenchH _ xs = xs
+-- -- Assume ms are sorted.
+-- sliceTrenchH :: [Int] -> Trench -> Trench
+-- sliceTrenchH ms ((m1 :. n1) : (m2 :. n2) : xs) =
+--   (m1 :. n1) : additionalPointsCorrectOrder ++ sliceTrenchH ms ((m2 :. n2) : xs)
+--   where
+--     msSlicing = filter (\m -> slices m m1 m2) ms
+--     additionalPoints = [m :. n1 | m <- msSlicing]
+--     additionalPointsCorrectOrder =
+--       if m1 < m2
+--         then additionalPoints
+--         else reverse additionalPoints
+-- sliceTrenchH _ xs = xs
 
 data Area = Inside Int | Outside
 
--- Assume xs are sorted by columns.
-areaOfSlice :: Int -> Int -> Trench -> Int
-areaOfSlice from to xs = go Outside pointsAtA pointsAtB
+type VerticalLine = (Ix2, Ix2)
+
+getVerticalLines :: Trench -> [VerticalLine]
+getVerticalLines (x : y : zs)
+  | A.lastDim x == A.lastDim y = (x, y) : getVerticalLines (y : zs)
+  | otherwise = getVerticalLines (y : zs)
+getVerticalLines _ = []
+
+sort2 :: Int -> Int -> (Int, Int)
+sort2 a b
+  | a <= b = (a, b)
+  | otherwise = (b, a)
+
+coversVerticalLine :: Int -> Int -> VerticalLine -> Bool
+coversVerticalLine a b (c :. _, d :. _) = c' <= a' && b' <= d'
   where
-    pointsAtA = traceShowId $ filter ((== from) . A.headDim) xs
-    pointsAtB = traceShowId $ filter ((== to) . A.headDim) xs
-    go area as@(a : asTail) bs@(b : bsTail)
-      -- Vertical line.
-      | A.lastDim a == A.lastDim b = case area of
-          (Inside n) -> (A.lastDim a - n) * (to - from) + go Outside asTail bsTail
-          Outside -> go (Inside $ A.lastDim a) asTail bsTail
-      | A.lastDim a < A.lastDim b = go area asTail bs
-      | A.lastDim a > A.lastDim b = go area as bsTail
-    go (Inside _) _ _ = error "areaOfSlice: inside but no more points"
-    go _ _ _ = 0
+    (a', b') = sort2 a b
+    (c', d') = sort2 c d
+
+getColOfVerticalLine :: VerticalLine -> Int
+getColOfVerticalLine (a, _) = A.lastDim a
+
+-- Assume xs are sorted by columns.
+areaOfSlice :: Int -> Int -> [VerticalLine] -> Int
+areaOfSlice from to xs = traceShow from $ traceShow to $ go Outside linesCoveringAToB
+  where
+    linesCoveringAToB = traceShowId $ filter (coversVerticalLine from to) xs
+    go area (l : ls) = case area of
+      (Inside n) -> (getColOfVerticalLine l - n + 1) * (to - from + 1) + go Outside ls
+      Outside -> go (Inside $ getColOfVerticalLine l) ls
+    go (Inside _) [] = error "areaOfSlice: inside but no more points"
+    go _ _ = 0
+
+getSliceIntervals :: [Int] -> [(Int, Int)]
+getSliceIntervals (a : b : ms)
+  | b - a > 1 =
+      (a, a)
+        : (succ a, pred b)
+        : getSliceIntervals (b : ms)
+  | b - a == 1 = (a, a) : getSliceIntervals (b : ms)
+  | otherwise = error "getSliceIntervals: forgot nub"
+getSliceIntervals [a] = [(a, a)]
+getSliceIntervals [] = []
 
 main :: IO ()
 main = do
@@ -223,8 +255,9 @@ main = do
   -- is2 <- parseChallengeT (Sample 18 1) pDigPlanColor
   let trenchCorners = reverse $ excavate2 is1
   let ms = nub $ sort $ map A.headDim trenchCorners
-      trenchWithSlices = sliceTrenchH ms trenchCorners
-      sortedTrenchWithSlices = nub $ sortOn A.lastDim trenchWithSlices
-      sliceIntervals = zip ms (tail ms)
-      areas = [areaOfSlice a b sortedTrenchWithSlices | (a, b) <- sliceIntervals]
+      verticalLines = getVerticalLines trenchCorners
+      sortedVerticalLines = nub $ sortOn (A.lastDim . fst) verticalLines
+      sliceIntervals = getSliceIntervals ms
+      areas = [areaOfSlice a b sortedVerticalLines | (a, b) <- sliceIntervals]
+  print sliceIntervals
   print areas
