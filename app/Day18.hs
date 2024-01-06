1309804f7d662f7f8d3fa4aeb8719d2afffaae73
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-01-06 22:12:00 +0100

    fix day18 a bit, but still ugly :)

--- app/Day18.hs
+++ app/Day18.hs
@@ -19,10 +19,8 @@ module Main
 where
 
 import Aoc
-import Aoc.Bounded (predWrap, succWrap)
 import Aoc.Direction
   ( Direction (..),
-    directionToIx2,
     ix2ToDirections,
     moveNStepsInDirection,
     turnLeft,
@@ -43,14 +41,10 @@ import Data.Attoparsec.Text
     skipWhile,
     string,
   )
-import Data.Bifunctor (Bifunctor (..))
 import Data.Char (isHexDigit)
 import Data.List (nub, sort, sortOn)
-import Data.Massiv.Array (Ix2 (..), Sz (..))
+import Data.Massiv.Array (Ix2 (..))
 import qualified Data.Massiv.Array as A
-import Data.Set (Set)
-import qualified Data.Set as S
-import Debug.Trace
 import GHC.Natural (Natural)
 
 data Instruction = Instruction {direction :: Direction, edgeLength :: Natural}
@@ -79,79 +73,6 @@ pDigPlan = pInstruction `sepBy1'` endOfLine <* endOfLine <* endOfInput
 
 type Trench = [Ix2]
 
-excavate :: [Instruction] -> Trench
-excavate = snd . foldl accF (0 :. 0, [])
-  where
-    accF (pos, trench) (Instruction dir len) =
-      let poss = getPositions pos dir len
-          trench' = trench ++ poss
-       in (last poss, trench')
-    getPositions pos dir len =
-      [ moveNStepsInDirection (fromIntegral n) pos dir
-        | n <- [1 .. len]
-      ]
-
-getDimensions :: Trench -> (Ix2, Ix2)
-getDimensions xs = (topLeft, bottomRight)
-  where
-    ms = map (fst . A.fromIx2) xs
-    ns = map (snd . A.fromIx2) xs
-    topLeft = minimum ms :. minimum ns
-    bottomRight = maximum ms :. maximum ns
-
-getLoopWithDirections :: [Ix2] -> [(Ix2, Direction)]
-getLoopWithDirections (x : y : z : xs) = [(y, d) | d <- directions] ++ goOn
-  where
-    directions = ix2ToDirections $ z - x
-    goOn = getLoopWithDirections (y : z : xs)
-getLoopWithDirections _ = []
-
-raytraceUntil :: Sz Ix2 -> Set Ix2 -> Ix2 -> Direction -> [Ix2]
-raytraceUntil sz lp ix dir
-  | ix' `S.member` lp || not (A.isSafeIndex sz ix') = []
-  | otherwise = ix' : raytraceUntil sz lp ix' dir
-  where
-    ix' = ix + directionToIx2 dir
-
-ixsLeftOf :: Sz Ix2 -> Set Ix2 -> Ix2 -> Direction -> [Ix2]
-ixsLeftOf sz lp ix dir = raytraceUntil sz lp ix (predWrap dir)
-
-ixsRightOf :: Sz Ix2 -> Set Ix2 -> Ix2 -> Direction -> [Ix2]
-ixsRightOf sz lp ix dir = raytraceUntil sz lp ix (succWrap dir)
-
-getRightAndLeftTiles :: Sz Ix2 -> Set Ix2 -> [(Ix2, Direction)] -> ([Ix2], [Ix2])
-getRightAndLeftTiles sz lp = go [] []
-  where
-    go lefts rights [] = (lefts, rights)
-    go lefts rights ((x, dir) : xs) =
-      go
-        (ixsLeftOf sz lp x dir ++ lefts)
-        (ixsRightOf sz lp x dir ++ rights)
-        xs
-
-atBorder :: Sz Ix2 -> Ix2 -> Bool
-atBorder (Sz (rows :. cols)) (m :. n) = m == 0 || n == 0 || m == pred rows || n == pred cols
-
-isOutside :: Sz Ix2 -> [Ix2] -> Bool
-isOutside sz = any (atBorder sz)
-
-getSize :: Trench -> Int
-getSize trenchShifted =
-  let (topLeftShifted, _) = getDimensions trenchShifted
-      trench = map (\k -> k - topLeftShifted) trenchShifted
-      (_, bottomRight) = getDimensions trench
-      trenchSize = Sz $ bottomRight + (1 :. 1)
-      trenchPathLoop = last trench : trench
-      trenchPathLoopWithDirection = getLoopWithDirections trenchPathLoop
-      (leftTiles, rightTiles) =
-        bimap nub nub $
-          getRightAndLeftTiles trenchSize (S.fromList trenchPathLoop) trenchPathLoopWithDirection
-      inside = length $ case (isOutside trenchSize leftTiles, isOutside trenchSize rightTiles) of
-        (True, False) -> rightTiles
-        (False, True) -> leftTiles
-        _ -> error "could not determine inside"
-   in inside + length trench
-
 pDirectionInt :: Parser Direction
 pDirectionInt =
   choice
@@ -177,8 +98,8 @@ pColorInstruction = do
 pDigPlanColor :: Parser [Instruction]
 pDigPlanColor = pColorInstruction `sepBy1'` endOfLine <* endOfLine <* endOfInput
 
-excavate2 :: [Instruction] -> Trench
-excavate2 = foldl accF [0 :. 0]
+excavate :: [Instruction] -> Trench
+excavate = reverse . foldl accF [0 :. 0]
   where
     accF (pos : trench) (Instruction dir len) =
       let pos' = moveNStepsInDirection (fromIntegral len) pos dir
@@ -186,22 +107,14 @@ excavate2 = foldl accF [0 :. 0]
        in trench'
     accF [] _ = error "no current position"
 
-slices :: Int -> Int -> Int -> Bool
-slices x a b = (a < x && x < b) || (a > x && x > b)
-
 data Area = Inside Int | Outside
 
 type VerticalLine = (Ix2, Ix2)
 
-getVerticalLines :: Trench -> [VerticalLine]
-getVerticalLines (x : y : zs)
-  | A.lastDim x == A.lastDim y = (x, y) : getVerticalLines (y : zs)
-  | otherwise = getVerticalLines (y : zs)
-getVerticalLines _ = []
-
 data Turn = TurnLeft | TurnRight
   deriving (Show, Eq)
 
+turnOutside :: Turn
 turnOutside = TurnLeft
 
 getVerticalLinesWithTurns :: [(Ix2, Turn)] -> [VerticalLine]
@@ -229,9 +142,9 @@ getColOfVerticalLine (a, _) = A.lastDim a
 
 -- Assume xs are sorted by columns.
 areaOfSlice :: Int -> Int -> [VerticalLine] -> Int
-areaOfSlice from to xs = traceShow from $ traceShow to $ go Outside linesCoveringAToB
+areaOfSlice from to xs = go Outside linesCoveringAToB
   where
-    linesCoveringAToB = traceShowId $ filter (coversVerticalLine from to) xs
+    linesCoveringAToB = filter (coversVerticalLine from to) xs
     go area (l : ls) = case area of
       (Inside n) -> (getColOfVerticalLine l - n + 1) * (to - from + 1) + go Outside ls
       Outside -> go (Inside $ getColOfVerticalLine l) ls
@@ -258,29 +171,31 @@ getTrenchWithTurns' :: [Ix2] -> [(Ix2, Turn)]
 getTrenchWithTurns' (x : y : z : zs) =
   (y, directionsToTurn a b) : getTrenchWithTurns' (y : z : zs)
   where
-    a = head $ ix2ToDirections (traceShowId y - traceShowId x)
+    a = head $ ix2ToDirections (y - x)
     b = head $ ix2ToDirections (z - y)
 getTrenchWithTurns' _ = []
 
 getTrenchWithTurns :: [Ix2] -> [(Ix2, Turn)]
-getTrenchWithTurns xs = getTrenchWithTurns' $ traceShowId $ before : xs ++ [after]
+getTrenchWithTurns xs = getTrenchWithTurns' $ before : xs ++ [after]
   where
     before = last (init xs)
     after = head $ tail xs
 
+getArea :: Trench -> Int
+getArea trenchCorners = sum areas
+  where
+    trenchCornersWithTurns = getTrenchWithTurns trenchCorners
+    verticalLines = getVerticalLinesWithTurns trenchCornersWithTurns
+    sortedVerticalLines = nub $ sortOn (A.lastDim . fst) verticalLines
+    ms = nub $ sort $ map A.headDim trenchCorners
+    sliceIntervals = getSliceIntervals ms
+    areas = [areaOfSlice a b sortedVerticalLines | (a, b) <- sliceIntervals]
+
 main :: IO ()
 main = do
-  is1 <- parseChallengeT (Sample 18 1) pDigPlan
-  let trenchShifted = excavate is1
-  print $ getSize trenchShifted
+  is1 <- parseChallengeT (Full 18) pDigPlan
+  let trenchCorners1 = excavate is1
+  print $ getArea trenchCorners1
   is2 <- parseChallengeT (Full 18) pDigPlanColor
-  let trenchCorners = reverse $ excavate2 is2
-      trenchCornersWithTurns = getTrenchWithTurns trenchCorners
-      verticalLines = getVerticalLinesWithTurns trenchCornersWithTurns
-      sortedVerticalLines = nub $ sortOn (A.lastDim . fst) verticalLines
-      ms = nub $ sort $ map A.headDim trenchCorners
-      sliceIntervals = getSliceIntervals ms
-      areas = [areaOfSlice a b sortedVerticalLines | (a, b) <- sliceIntervals]
-  print verticalLines
-  print areas
-  print $ sum areas
+  let trenchCorners2 = excavate is2
+  print $ getArea trenchCorners2
