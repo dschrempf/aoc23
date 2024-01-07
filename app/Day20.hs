019440254ebae668413beb93104bf695b2b471a7
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-01-07 22:49:54 +0100

    day 20, parser

--- app/Day20.hs
+++ app/Day20.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module      :  Main
 -- Description :  Day 20
@@ -16,5 +18,75 @@ module Main
   )
 where
 
+import Aoc
+import Control.Applicative (Alternative (..))
+import Data.Attoparsec.Text
+  ( Parser,
+    char,
+    endOfLine,
+    sepBy1',
+    string,
+    takeWhile1,
+  )
+import Data.Char (isAlpha)
+import Data.Map (Map)
+import qualified Data.Map as M
+
+data Pulse = Low | High
+  deriving (Show, Eq)
+
+data FlipFlopState = On | Off
+  deriving (Show, Eq)
+
+type ModuleName = Text
+
+type Input = Text
+
+type Destination = Text
+
+data ModuleType
+  = Broadcaster
+  | FlipFlop FlipFlopState
+  | Conjunction (Map Input Pulse)
+  deriving (Show, Eq)
+
+data Module = Module
+  { moduleType :: ModuleType,
+    moduleName :: ModuleName,
+    destinations :: [Destination]
+  }
+  deriving (Show, Eq)
+
+pModuleName :: Parser Text
+pModuleName = takeWhile1 isAlpha
+
+pDestinations :: Parser [Destination]
+pDestinations = pModuleName `sepBy1'` string ", "
+
+pStandardModuleWith :: ModuleType -> Parser Module
+pStandardModuleWith mt =
+  Module mt
+    <$> pModuleName
+    <*> (string " -> " *> pDestinations)
+
+pBroadcaster :: Parser Module
+pBroadcaster = pStandardModuleWith Broadcaster
+
+pFlipFlop :: Parser Module
+pFlipFlop = char '%' *> pStandardModuleWith (FlipFlop Off)
+
+pConjunction :: Parser Module
+pConjunction = char '&' *> pStandardModuleWith (Conjunction M.empty)
+
+pModule :: Parser Module
+pModule = pBroadcaster <|> pFlipFlop <|> pConjunction
+
+pInput :: Parser (Map ModuleName Module)
+pInput = M.fromList . map extractModuleName <$> pModule `sepBy1'` endOfLine
+  where
+    extractModuleName m = (moduleName m, m)
+
 main :: IO ()
-main = undefined
+main = do
+  ms <- parseChallengeT (Sample 20 1) pInput
+  print ms
