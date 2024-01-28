be713136b141bd495889772c29563cd6f1d3de21
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-01-28 16:20:23 +0100

    day 22, part 2

--- app/Day22.hs
+++ app/Day22.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 -- |
 -- Module      :  Main
 -- Description :  Day 22
@@ -19,20 +21,24 @@ where
 import Aoc
 import Aoc.Tuple (sortTuple)
 import Data.Attoparsec.Text (Parser, char, decimal, endOfLine, sepBy1')
+import Data.Hashable (Hashable (..))
 import Data.Set (Set)
 import qualified Data.Set as S
+import GHC.Generics (Generic)
 
 data Point = Point {xPos :: Int, yPos :: Int, zPos :: Int}
-  deriving (Show, Eq, Read)
+  deriving (Show, Eq, Read, Generic)
 
 instance Ord Point where
   x `compare` y = (zPos x, xPos x, yPos x) `compare` (zPos y, xPos y, yPos y)
 
-data Brick = Brick {from :: Point, to :: Point}
+instance Hashable Point
+
+data Brick = Brick {from :: Point, to :: Point, hashValue :: Int}
   deriving (Show, Eq, Read)
 
 instance Ord Brick where
-  x `compare` y = (to x, from x) `compare` (to y, from y)
+  x `compare` y = (to x, from x, hashValue x) `compare` (to y, from y, hashValue y)
 
 pPoint :: Parser Point
 pPoint = Point <$> decimal <* char ',' <*> decimal <* char ',' <*> decimal
@@ -43,7 +49,7 @@ pBrick = do
   _ <- char '~'
   p2 <- pPoint
   let (f, t) = sortTuple (p2, p1)
-  pure $ Brick f t
+  pure $ Brick f t (hash (f, t))
 
 pInput :: Parser (Set Brick)
 pInput = S.fromList <$> pBrick `sepBy1'` endOfLine
@@ -52,7 +58,7 @@ fallPoint :: Int -> Point -> Point
 fallPoint n (Point x y z) = Point x y (z - n)
 
 fallBrick :: Int -> Brick -> Brick
-fallBrick n (Brick f t) = Brick (fallPoint n f) (fallPoint n t)
+fallBrick n (Brick f t h) = Brick (fallPoint n f) (fallPoint n t) h
 
 overlap :: (Int, Int) -> (Int, Int) -> Bool
 overlap (a', b') (c', d') = a <= d && b >= c
@@ -61,7 +67,7 @@ overlap (a', b') (c', d') = a <= d && b >= c
     (c, d) = sortTuple (c', d')
 
 crosses :: Brick -> Brick -> Bool
-crosses (Brick f1 t1) (Brick f2 t2) =
+crosses (Brick f1 t1 _) (Brick f2 t2 _) =
   overlap (xPos f1, xPos t1) (xPos f2, xPos t2)
     && overlap (yPos f1, yPos t1) (yPos f2, yPos t2)
 
@@ -77,13 +83,27 @@ fallOnTop xs b = let b' = fallBrick (zOfFallingBrick - zOfHighestBrick - 1) b in
 fall :: Set Brick -> Set Brick
 fall = S.foldl' fallOnTop S.empty
 
-findNonSupporters :: Set Brick -> Set Brick
-findNonSupporters xs = S.filter predicate xs
+-- -- That's how I solved part one originally.
+-- findNonSupporters :: Set Brick -> Set Brick
+-- findNonSupporters xs = S.filter predicate xs
+--   where
+--     predicate x = let xs' = S.delete x xs in xs' == fall xs'
+
+findNumberOfFallers :: Set Brick -> [Int]
+findNumberOfFallers xs = map nFallers $ S.toList xs
   where
-    predicate x = let xs' = S.delete x xs in xs' == fall xs'
+    nFallers x =
+      let xs' = S.delete x xs
+          xs'' = fall xs'
+       in if fall xs'' /= xs''
+            then error ""
+            else S.size $ xs'' S.\\ xs'
 
 main :: IO ()
 main = do
   bs <- parseChallengeT (Full 22) pInput
   let fallenBs = fall bs
-  print $ S.size $ findNonSupporters fallenBs
+  let fallers = findNumberOfFallers fallenBs
+  -- Part 1.
+  print $ length $ filter (== 0) fallers
+  print $ sum $ findNumberOfFallers fallenBs
